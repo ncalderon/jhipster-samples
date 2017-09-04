2bd35991a566548f846ddcbee75694436dd19f50
ncalderon <nathaniel.calderon@gmail.com>
2017-09-04 16:10:43 -0400

    Upload project

---
+++ blog/src/main/java/com/company/blog/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.company.blog.security;
+
+import com.company.blog.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return userName != null ? userName : Constants.SYSTEM_ACCOUNT;
+    }
+}
---
+++ blog/src/main/java/com/company/blog/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.company.blog.service.dto;
